7d951d2bc054f9f2712b60278ae150e0b4b3db6c
Mickaël <mickael.baye@gmail.com>
2017-12-05 16:19:55 +0100

    feat: init foodtruck list page, foodtrucks desc and subtitle

--- src/app/foodtruck.ts
+++ src/app/foodtruck.ts
@@ -2,4 +2,6 @@ export class Foodtruck {
   id: number;
   name: string;
   days: number[];
+  description: string;
+  subtitle: string;
 }
--- src/app/mock-foodtrucks.ts
+++ src/app/mock-foodtrucks.ts
@@ -4,36 +4,57 @@ export const FOODTRUCKS: Foodtruck[] = [
   {
 		id : 0,
 		name : "So Faim",
-		days : [1, 3]
+    days : [1, 3],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 1,
 		name : "Le camion à croquer",
-		days : [2]
+    days : [2],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 2,
 		name : "Galette",
-		days : [2]
+    days : [2],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 3,
 		name : "Nomad Kitchen",
-		days : [3, 5]
+    days : [3, 5],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 4,
 		name : "Rotisserie",
-		days : [4]
+    days : [4],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 5,
 		name : "Kig",
-		days : [4]
+    days : [4],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	},
 	{
 		id : 6,
 		name : "Pizza",
-		days : [5]
+    days : [5],
+    description : "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum\
+    nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
+    subtitle : "Card subtitle"
 	}
 ];
