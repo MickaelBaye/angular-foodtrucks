c53acd592a1d23f8dada95418a8efb6d33fd644b
Mickaël <mickael.baye@gmail.com>
2017-12-07 14:41:15 +0100

    clean: projet tour of hereos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
-import { HeroService }          from './hero.service';
-import { MessageService }       from './message.service';
-import { MessagesComponent }    from './messages/messages.component';
 import { FoodtrucksComponent }  from './foodtrucks/foodtrucks.component';
 import { FoodtruckService }     from './foodtruck.service';
 import { HomeComponent }        from './home/home.component';
@@ -30,29 +22,17 @@ import { WeekComponent } from './week/week.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    MomentModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    MomentModule
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent,
     FoodtrucksComponent,
     HomeComponent,
     FoodtruckDetailComponent,
     TodayComponent,
     WeekComponent
   ],
-  providers: [ HeroService, MessageService, FoodtruckService ],
+  providers: [ FoodtruckService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
