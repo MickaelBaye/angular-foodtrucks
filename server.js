a565660fbbf38e58d39741f82d284963fbf64a6c
Mickael Baye <mickael.baye@gmail.com>
2018-03-12 19:53:38 +0100

    feat: init mongodb integration

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 const path = require('path');
 var bodyParser = require("body-parser");
-// var mongodb = require("mongodb");
+var mongodb = require("mongodb");
 
 var APPLICATION_PORT = 8080;
-// var ObjectID = mongodb.ObjectID;
+var MONGODB_URI = "mongodb://localhost:27017/foodtrucks";
+var ObjectID = mongodb.ObjectID;
 // var CONTACTS_COLLECTION = "contacts";
+var FOODTRUCKS_COLLECTION = "foodtrucks";
 
 var app = express();
 app.use(bodyParser.json());
@@ -21,30 +23,24 @@ app.get('*', (req, res) => {
 
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-// var db;
+var db;
 
 // Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
+mongodb.MongoClient.connect(process.env.MONGODB_URI || MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-//   // Save database object from the callback for reuse.
-//   db = database;
-//   console.log("Database connection ready");
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-//   // Initialize the app.
-//   var server = app.listen(process.env.PORT || 8080, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
-
-// Initialize the app.
-var server = app.listen(process.env.PORT || APPLICATION_PORT, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || APPLICATION_PORT, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
 // API ROUTES BELOW
